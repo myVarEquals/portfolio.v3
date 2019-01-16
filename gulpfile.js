e9f15f0bf5f12812e8dc1c35969a6f39db4b1c6f
Roger B <myvarequals@gmail.com>
2019-01-16 01:12:32 -0500

    testing gulp packages

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,38 @@
 const gulp = require('gulp');
-const browserSync = require('browser-sync').create();
-const sass = require('sass');
+const imagemin = require('gulp-imagemin');
+const sass = require('gulp-sass');
+sass.compiler = require('node-sass');
+const uglify = require('gulp-uglify');
+const babel = require('gulp-babel');
 
-gulp.task('sass', () => { 
-    return gulp.src(['node_modules/bootstrap/scss/bootstrap.scss', 'src/scss/*.scss']) // bootstrap and sass
-        .pipe(sass())
-        .pipe(gulp.dest('src/css'))
-        .pipe(browserSync.stream());
-});
+gulp.task('html', () => 
+    gulp.src('./src/*.html')
+        .pipe(gulp.dest('dist'))
+);
+
+gulp.task('sass', () =>
+    gulp.src('./src/sass/*.sass')
+        .pipe(sass().on('error', sass.logError))
+        .pipe(gulp.dest('./dist/css'))
+);
+
+
+gulp.task('babel', () =>
+    gulp.src('./src/js/*.es6')
+        .pipe(babel({
+            presets: ['@babel/env']
+        }))
+        .pipe(gulp.dest('./src/js'))
+);
+
+gulp.task('uglifyJS', () =>
+    gulp.src('./src/js/*.js')
+        .pipe(uglify())
+        .pipe(gulp.dest('dist/js'))
+);
+
+gulp.task('imagemin', () => 
+    gulp.src('./src/assets/images/*')
+        .pipe(imagemin())
+        .pipe(gulp.dest('./dist/assets/images'))
+);
---
+++ src/js/index.js
@@ -0,0 +1,4 @@
+"use strict";
+
+var hello = "yo";
+var world = "planet";
